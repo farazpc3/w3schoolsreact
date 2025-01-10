7ceb57850ddd15b1117ef561482d144ec34486df
farazpc3 <farazpc3@gmail.com>
2025-01-10 10:34:12 +0330

    react init

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "w3schools-react",
+  base: "/w3schoolsreact",
 })
